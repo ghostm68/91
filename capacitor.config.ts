f47a6528c5c34d4ad7c3fa65b957fcaa94b6eacd
inky <42044862+ghostm68@users.noreply.github.com>
2025-10-19 11:22:57 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,31 +1,14 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/core';
 
 const config: CapacitorConfig = {
-  appId: 'com.lovable.inkrealm',
-  appName: 'InkRealm',
+  appId: 'app.lovable.inkrealm.mirror',
+  appName: '1991',
   webDir: 'dist',
-  // Commented out for production builds - use built files instead of dev server
-  // server: {
-  //   url: 'https://04c29243-e6b8-4494-a980-bd97615ec68d.lovableproject.com?forceHideBadge=true',
-  //   cleartext: true
-  // },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: '#000000',
-      showSpinner: false
-    }
-  },
   android: {
-    icon: {
-      iconPath: 'src/assets/app-icon.jpeg'
-    }
+    allowMixedContent: true,
+    backgroundColor: '#0D0D0D'
   },
-  ios: {
-    icon: {
-      iconPath: 'src/assets/app-icon.jpeg'
-    }
-  }
+  icon: 'icon.png'
 };
 
 export default config;
